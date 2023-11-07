6fd610c729e25939eb77f1433448cf7e299cee9a
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-08 00:13:22 +0800

    feat: add POT priority queue exercise

--- DSA/Priority Queues/POTPqueue.c
+++ DSA/Priority Queues/POTPqueue.c
@@ -2,7 +2,13 @@
 
 // * FUNCTION DEFINITIONS HERE
 void Insert(int elem, pQueue *q){
-
+    int x, temp, index = ++q->lastIndex;
+    q->heap[index] = elem;
+    while(q->heap[index] > q->heap[(index - 1)/2]){
+        temp = q->heap[(index - 1)/2];
+        q->heap[(index - 1)/2] = q->heap[index];
+        q->heap[index] = temp;
+    }
 }
 
 void Display(pQueue q){
